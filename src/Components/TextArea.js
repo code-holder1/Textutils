8e7da3f48a1487b6c93ab1a9d9169f42f0c8c5ea
code-holder1 <codeholder1@gmail.com>
2025-05-28 17:55:31 +0500

    minor fixes

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-
 export default function TextArea(props) {
   const [text, setText] = useState("");
 
@@ -16,10 +15,7 @@ export default function TextArea(props) {
   };
 
   const copyText = () => {
-    var text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    document.getSelection().removeAllRanges();
+    navigator.clipboard.writeText(text);
     props.getAlert("Text copied to clipboard", "success");
   };
 
@@ -60,31 +56,66 @@ export default function TextArea(props) {
           value={text}
           onChange={Change}
         />
-        <button disabled={text.length === 0}  className="btn btn-primary my-1" onClick={UpCase}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary my-1"
+          onClick={UpCase}
+        >
           comvert to Uppercase
         </button>
-        <button disabled={text.length === 0}  className="btn btn-primary m-1" onClick={lowerCase}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary m-1"
+          onClick={lowerCase}
+        >
           convert to Lowercase
         </button>
-        <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={extraSpaces}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary m-1"
+          onClick={extraSpaces}
+        >
           Remove Extra Spaces
         </button>
-        <button disabled={text.length === 0}  className="btn btn-primary m-1" onClick={copyText}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary m-1"
+          onClick={copyText}
+        >
           Copy Text
         </button>
-        <button disabled={text.length === 0}  className="btn btn-primary m-1" onClick={speak}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary m-1"
+          onClick={speak}
+        >
           Speak
         </button>
-        <button disabled={text.length === 0}  className="btn btn-primary m-1" onClick={clearText}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary m-1"
+          onClick={clearText}
+        >
           Clear
         </button>
       </div>
       <div className="container my-3">
         <h2>Your text summary</h2>
         <p>
-          {text.split(" ").filter((e)=>{return e.length!==0}).length} words and {text.length} characters
+          {
+            text.split(/\s/).filter((e) => {
+              return e.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
         </p>
-        <p>{0.008 * text.split(" ").filter((e)=>{return e.length!==0}).length} minutes to read</p>
+        <p>
+          {0.008 *
+            text.split(/\s/).filter((e) => {
+              return e.length !== 0;
+            }).length}{" "}
+          minutes to read
+        </p>
         <h2>Preview</h2>
         <p>
           {text.length > 0
